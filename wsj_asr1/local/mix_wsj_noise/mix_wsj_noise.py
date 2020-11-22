e21cdddf178443e876366c44cb7e5f4053c6e266
Justin Nguyen <justinnuwin@gmail.com>
2020-11-22 11:51:57 +0000

    Add support for randomized trials with multiple songs

--- wsj_asr1/local/mix_wsj_noise/mix_wsj_noise.py
+++ wsj_asr1/local/mix_wsj_noise/mix_wsj_noise.py
@@ -5,7 +5,7 @@ usage: mix_wsj_noise.py [-h] [--mix-snr snr] [--speech-level db]
                         [--noise-level db] [--mix-level db]
                         [--noise-timestamp time] [--noiseROI filepath]
                         [--sph2pipe path/to/sph2pipe] [--job]
-                        [--dry-run] dataPath noiseFile
+                        [--dry-run] [--noise-ext]dataPath noiseFile
 
 Note mp3 codec is normally not installed by default:
 $ sudo apt-get install libsox-fmt-mp3
@@ -16,6 +16,7 @@ import sys
 import argparse
 from math import log10
 import subprocess
+from random import randint
 
 
 BITDEPTH=16
@@ -41,10 +42,20 @@ def build_parser():
     parser.add_argument('dataPath', type=dir_path,
             help='Path to the dataset in the data directory (i.e. data/train_si284). \
                     Required files in this directory is wav.scp and utt2dur.')
-    parser.add_argument('noiseFile', type=file_path,
-            help='Path to the noise file. If there is no ROI mapping file or the \
-                    specified file is not in the ROI map, then a random slice will \
-                    be taken.',)
+    parser.add_argument('noiseFile', type=str,
+            help='Path to the noise file or directory containing several noise files. \
+                    If a file is given, all utterances will be mixed with the specified \
+                    file. If a directory is given, the utterances will be mixed with a \
+                    randomly selected noise source in this directory following a uniform \
+                    distribution. When using a directory, to search for noise sources, the \
+                    `--noise-ext` argument MUST be provided. A file will be generated mapping \
+                    the utterance ID to the noise source. If `--noise-timestamp` is not given, \
+                    or there is no ROI mapping file, or the specified file is not in the \
+                    ROI map, then start mixing from beginning.')
+    parser.add_argument('--noise-ext', type=str, metavar='ext',
+            help='Used only when noiseFile is a directory. This is used to search for noise files \
+                    of a specified extension in the noiseFile directory in case there are other \
+                    files in that folder.')
     parser.add_argument('--mix-snr', type=str, metavar='snr',
             help='Mix speech and noise at the specified SNR. snr can be given as db \
                     or a ratio. i.e. "2:1" would mix the speech signal at 2x the \
@@ -70,7 +81,7 @@ def build_parser():
     parser.add_argument('--noiseROI', type=file_path, metavar='filepath',
             help='Path to the noise.roi file. This file contains the mapping from \
                     noise filename (unique) to a list of regions of interest (start\
-                    /end times). This is superceded by --noise_timestamp. \
+                    /end times). This is superceded by --noise-timestamp. \
                     (ROI is region of interest)')
     parser.add_argument('--sph2pipe', type=file_path, metavar='path/to/sph2pipe',
             help='Path to sph2pipe if it is not on the path. This is needed to \
@@ -164,6 +175,17 @@ def prepare_sources(noiseFile_path, mix_snr, speech_level_str, noise_level_str):
     else:
         noiseWAV_path = match_noise_properties_to_speech(noiseFile_path, noise_level_str)
 
+    """ This hasn't been tested yet
+    command = 'soxi -D {}'.format(=noiseWAV_path)
+    return_code = subprocess.run(command, shell=True)
+    output = res.stdout.decode('utf-8')
+    try:
+        noise_length = float(output.strip())
+    except Exception:
+        raise ValueError('Could not get length of noise-source')
+    """
+    noise_length = None
+
     if speech_level_str is not None:
         speech_level, relative = parse_level_str(speech_level_str)
         if relative:
@@ -173,13 +195,25 @@ def prepare_sources(noiseFile_path, mix_snr, speech_level_str, noise_level_str):
     else:
         speech_gain_effect = 'gain -n'
 
-    return noiseWAV_path, speech_gain_effect
+    return noiseWAV_path, speech_gain_effect, noise_length
 
 
-def main(wavscp_path, utt2dur_path, noiseFile_path, mix_snr=None, speech_level_str=None, noise_level_str=None,
+def main(wavscp_path, utt2dur_path, noiseFile_path, noise_ext=None, mix_snr=None, speech_level_str=None, noise_level_str=None,
         mix_level=None, noise_timestamp=None, noiseROI_path=None, dry_run=False, sph2pipe=None):
 
-    noiseWAV_path, speech_gain_effect = prepare_sources(noiseFile_path, mix_snr, speech_level_str, noise_level_str)
+    noiseWAV_path = []
+    if noise_ext is not None:   # Directory mode
+        noise_mode = 'directory'
+        for song in sorted(os.listdir(noiseFile_path)):
+            for filename in sorted(os.listdir(os.path.join(noiseFile_path, song))):
+                if filename.endswith(noise_ext):
+                    _noiseWAV_path, speech_gain_effect, noise_length = prepare_sources(os.path.join(noiseFile_path, song, filename),
+                            mix_snr, speech_level_str, noise_level_str)
+                    noiseWAV_path.append(_noiseWAV_path)
+    else:
+        noise_mode = 'file'
+        _noiseWAV_path, speech_gain_effect, noise_length = prepare_sources(noiseFile_path, mix_snr, speech_level_str, noise_level_str)
+        noiseWAV_path.append(_noiseWAV_path)
 
     if noiseROI_path is not None:
         raise NotImplementedError('noise.roi file has not been implemented yet!')   # TODO
@@ -188,6 +222,10 @@ def main(wavscp_path, utt2dur_path, noiseFile_path, mix_snr=None, speech_level_s
 
     wavscp_f = open(wavscp_path, 'r')
     utt2dur_f = open(utt2dur_path, 'r')
+    if noise_mode == 'directory':
+        noise_utt_map_path = os.path.join(os.path.dirname(wavscp_path), 'noise_utt_map')
+        noise_utt_map_f = open(noise_utt_map_path, 'w')
+        noise_utt_map_f.write(str(noiseWAV_path) + '\n')
 
     if not dry_run:
         new_wavscp_path = os.path.join(os.path.dirname(wavscp_path), 'augmented_{}'.format(
@@ -210,12 +248,18 @@ def main(wavscp_path, utt2dur_path, noiseFile_path, mix_snr=None, speech_level_s
         else:
             this_mix_level = mix_level
 
+        if noise_mode == 'file':
+            noise_idx = 0
+        elif noise_mode == 'directory':
+            noise_idx = randint(0, len(noiseWAV_path) - 1)
+            noise_utt_map_f.write('{uttID}\t{noiseIdx}\n'.format(uttID=utt_id, noiseIdx=noise_idx))
+
         # Mix both inputs at 1/n balance factor
         # -p is --sox-pipe, for some reason it won't take that version of the argument
         augmented_command = 'sox -t wav - -p {speechEffect} | ' \
                             'sox --combine mix -p "|sox {noisePath} -p trim {start} {duration}" ' \
                             '-t wav -b {bit} -e {enc} - gain -n {mixLevel} |'.format(
-                speechEffect=speech_gain_effect, noisePath=noiseWAV_path, start=noise_timestamp,
+                speechEffect=speech_gain_effect, noisePath=noiseWAV_path[noise_idx], start=noise_timestamp,
                 duration=duration, bit=BITDEPTH, enc=ENCODING, mixLevel=this_mix_level)
 
         augmented_wavscp_line = '{} {}\n'.format(wavscp_line, augmented_command)
@@ -224,6 +268,8 @@ def main(wavscp_path, utt2dur_path, noiseFile_path, mix_snr=None, speech_level_s
     new_wavscp_f.close()
     wavscp_f.close()
     utt2dur_f.close()
+    if noise_mode == 'directory':
+        noise_utt_map_f.close()
 
 
 if __name__ == '__main__':
@@ -239,8 +285,30 @@ if __name__ == '__main__':
         raise FileNotFoundError('Could not find wav.scp in {}'.format(args.dataPath))
     if not os.path.isfile(utt2dur_path):
         raise FileNotFoundError('Could not find utt2dur in {}'.format(args.dataPath))
+    
+    if os.path.isdir(args.noiseFile):
+        if args.noise_ext is None:
+            raise ValueError('When noiseFile is a directory, the `--noise-ext` argument must be provided')
+        else:
+            if args.noise_ext[0] == '.':
+                noise_ext = args.noise_ext[1:]
+            else:
+                noise_ext = args.noise_ext
+            for song in os.listdir(args.noiseFile):
+                has_audio = False
+                for filename in os.listdir(os.path.join(args.noiseFile, song)):
+                    if filename.endswith(noise_ext):
+                        has_audio = True
+                        break
+                if not has_audio:
+                    raise FileNotFoundError('No files with the {} extension were found in the noiseFile folder'.format(
+                        noise_ext))
+    elif os.path.isfile(args.noiseFile):
+        noise_ext = None
+    else:
+        raise TypeError('Given noisePath was not a file nor a directory: {}'.format(args.noiseFile))
 
-    main(wavscp_path, utt2dur_path, args.noiseFile, mix_snr=args.mix_snr,
+    main(wavscp_path, utt2dur_path, args.noiseFile, noise_ext=noise_ext, mix_snr=args.mix_snr,
             speech_level_str=args.speech_level, noise_level_str=args.noise_level,
             mix_level=args.mix_level, noise_timestamp=args.noise_timestamp,
             noiseROI_path=args.noiseROI, dry_run=args.dry_run, sph2pipe=args.sph2pipe)
--- wsj_asr1/local/mix_wsj_noise/test/reset-test.sh
+++ wsj_asr1/local/mix_wsj_noise/test/reset-test.sh
@@ -1,1 +1,2 @@
-rm -f lv*.wav data/*wav.scp.* data/augmented_wav.scp
+rm -f data/*wav.scp.* data/augmented_wav.scp
+find noise | grep --color=never -E "lv.+wav$" | xargs -I {} rm -f 
